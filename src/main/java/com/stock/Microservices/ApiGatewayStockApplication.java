e3e264cd8cab9ecb99a931a9059bdbc9892ae2fa
Jackson <jacksonsamuelm10@gmail.com>
2024-10-25 21:12:23 +0530

    first commit

---
+++ src/main/java/com/stock/Microservices/ApiGatewayStockApplication.java
@@ -0,0 +1,13 @@
+package com.stock.Microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayStockApplication.class, args);
+	}
+
+}
